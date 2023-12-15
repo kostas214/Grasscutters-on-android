d203481c3d57842ce2da7dcc703f4e2f2bab0e54
kostas214 <100384475+kostas214@users.noreply.github.com>
2023-12-15 13:03:43 +0200

    Uploaded the Install script

---
+++ install.sh
@@ -0,0 +1,6 @@
+nix-env -iA nixpkgs.nano nixpkgs.mongodb nixpkgs.mongodb nixpkgs.jdk17 nixpkgs.mitmproxy nixpkgs.wget nixpkgs.curl
+wget https://github.com/Grasscutters/Grasscutter/releases/download/v1.7.4/grasscutter-1.7.4.jar
+git clone https://gitlab.com/YuukiPS/GC-Resources
+cd GC-Resources
+cd Resources
+mv  
