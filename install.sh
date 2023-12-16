d6d0571ab282b540bb21e5030ecbd6c142015e06
kostas214 <100384475+kostas214@users.noreply.github.com>
2023-12-16 03:41:54 +0200

    Update install.sh
    Made it actually do what its supposed to do

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 nix-env -iA nixpkgs.nano nixpkgs.mongodb nixpkgs.mongodb nixpkgs.jdk17 nixpkgs.mitmproxy nixpkgs.wget nixpkgs.curl
 wget https://github.com/Grasscutters/Grasscutter/releases/download/v1.7.4/grasscutter-1.7.4.jar
 git clone https://gitlab.com/YuukiPS/GC-Resources
-cd GC-Resources
-cd Resources
-mv  
+java -jar grasscutter-1.7.4.jar
+cd GC-Resources/Resources
+mv BinOutput/ ExcelBinOutput/ Scripts ScriptSceneData/ Server TextMap/ ~/Grasscutters-on-android/resources
+cd ~/Grasscutters-on-android
