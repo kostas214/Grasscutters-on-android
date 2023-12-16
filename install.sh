e85b50810aa95c18ec9d0f98242bb985f0a20b7a
kostas214 <100384475+kostas214@users.noreply.github.com>
2023-12-16 04:00:19 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-nix-env -iA nixpkgs.nano nixpkgs.mongodb nixpkgs.mongodb nixpkgs.jdk17 nixpkgs.mitmproxy nixpkgs.wget nixpkgs.curl
+nix-env -iA nixpkgs.nano nixpkgs.mongodb nixpkgs.jdk17 nixpkgs.mitmproxy nixpkgs.wget nixpkgs.curl
 wget https://github.com/Grasscutters/Grasscutter/releases/download/v1.7.4/grasscutter-1.7.4.jar
 git clone https://gitlab.com/YuukiPS/GC-Resources
 java -jar grasscutter-1.7.4.jar
